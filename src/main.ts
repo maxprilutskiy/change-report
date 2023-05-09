2256a80d38b047c2448f137f2e4737b225e4f7a0
maxprilutskiy <maks.prilutskiy@gmail.com>
2023-05-10 01:03:27 +0200

    chore: removed debug comment

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,6 @@ import {fetchCommitMessages} from './fetch-commit-messages'
 import {composeReport} from './compose-report'
 import {sendSlackMessage} from './send-slack-message'
 
-/**
- * This is the main entry point of the GitHub Action.
- * The github action accepts a number of days as a parameter called "days", name of the Slack channel as "channel",
- * and expects OPENAI_API_KEY, SLACK_BOT_TOKEN, and SLACK_SIGNING_SECRET, GITHUB_TOKEN to be set as an environment variables.
- * This github action retrieves all commit from the branch it runs on over the last "days" days,
- * uses OpenAI api to generate a human readable summary of the changes in markdown format,
- * and posts the summary to the Slack channel.
- */
-
 async function run(): Promise<void> {
   try {
     const daysCount = parseInt(core.getInput('days'))
